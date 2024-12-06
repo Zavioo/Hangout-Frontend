616e3289de1fede47d1762de268b97a4ebfc5286
Zavioo <saviotitus63@gmail.com>
2024-12-06 16:48:54 +0530

    registerCompleted

---
+++ src/Services/allApi.js
@@ -0,0 +1,8 @@
+import commonApi from "./commonApi"
+import SERVER_URL from "./serverURL"
+
+// registerAPI called by Authh
+export const registerAPI = async (reqBody) => {
+    return await commonApi("POST", `${SERVER_URL}/register`, reqBody)
+}
+
--- src/pages/Authh.jsx
+++ src/pages/Authh.jsx
@@ -1,25 +1,44 @@
 import React, { useState } from 'react'
 import login from '../assets/authimg/login.png'
 import register from '../assets/authimg/register.png'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { registerAPI } from '../Services/allApi'
+
 
 
 const Authh = ({ insideRegister }) => {
 
-    
+    const navigate = useNavigate()
 
     const [inputData, setInputData] = useState({
         username: '',
         name: '',
         email: '',
-        password: '',
+        password: ''
     })
 
-    console.log(inputData);
+    // console.log(inputData);
 
-    const handleRegister = () => {
+    const handleRegister = async (e) => {
+        e.preventDefault()
+        console.log("Inside Handle Register");
         if (inputData.username && inputData.name && inputData.email && inputData.password) {
-            alert('Make Api Call')
+            // alert('Make Api Call')
+            try {
+                const result = await registerAPI(inputData)
+                if (result.status == 200) {
+                    alert(`Welcome ${result.data.name}, Please Login to explore our website`)
+                    navigate('/userhome')
+                    setInputData({ username: "", name: "", email: "", password: "" })
+                } else {
+                    if (result.response.status == 406) {
+                        alert(result.response.data)
+                        setInputData({ username: "", name: "", email: "", password: "" })
+                    }
+                }
+            } catch (error) {
+                console.log(error);
+            }
 
         } else {
             alert('Please Fill All Fields')
@@ -38,7 +57,7 @@ const Authh = ({ insideRegister }) => {
 
     return (
         <>
-            <div className="tw-w-full tw-h-lvh tw-flex tw-justify-center tw-items-center " style={{backgroundColor:"rgb(243, 243, 243)"}} >
+            <div className="tw-w-full tw-h-lvh tw-flex tw-justify-center tw-items-center " style={{ backgroundColor: "rgb(243, 243, 243)" }} >
 
                 <div style={{ width: "900px", height: "506px" }} className="border tw-flex tw-justify-center tw-items-center rounded tw-shadow-lg tw-bg-white  "  >
                     <div className=' tw-w-1/2 ' >
@@ -50,16 +69,16 @@ const Authh = ({ insideRegister }) => {
                         <h3 className='tw-mb-3 tw-text-sm'> Sign {insideRegister ? "Up to create" : "In to your"} Account </h3>
 
                         {insideRegister && <input value={inputData.username} onChange={(e) => setInputData({ ...inputData, username: e.target.value })}
-                            type="text" placeholder="Username" class="tw-text-main border tw-p-1 rounded tw-h-9 tw-w-3/4 tw-mb-3 focus:tw-outline-none focus:tw-border-main " />}
+                            type="text" placeholder="Username" className="tw-text-main border tw-p-1 rounded tw-h-9 tw-w-3/4 tw-mb-3 focus:tw-outline-none focus:tw-border-main " />}
 
-                       { insideRegister &&  <input value={inputData.name} onChange={(e) => setInputData({ ...inputData, name: e.target.value })} type="text" placeholder="Name" class=" tw-text-main border tw-p-1 rounded tw-h-9 tw-w-3/4 tw-mb-3
+                        {insideRegister && <input value={inputData.name} onChange={(e) => setInputData({ ...inputData, name: e.target.value })} type="text" placeholder="Name" className=" tw-text-main border tw-p-1 rounded tw-h-9 tw-w-3/4 tw-mb-3
                         focus:tw-outline-none focus:tw-border-main" />}
 
-                        <input value={inputData.email} onChange={(e) => setInputData({ ...inputData, email: e.target.value })} type="email" placeholder="Email" class=" tw-text-main border tw-p-1 rounded tw-h-9 tw-w-3/4 tw-mb-3 focus:tw-outline-none focus:tw-border-main " />
+                        <input value={inputData.email} onChange={(e) => setInputData({ ...inputData, email: e.target.value })} type="email" placeholder="Email" className=" tw-text-main border tw-p-1 rounded tw-h-9 tw-w-3/4 tw-mb-3 focus:tw-outline-none focus:tw-border-main " />
 
-                        <input value={inputData.password} onChange={(e) => setInputData({ ...inputData, password: e.target.value })} type="password" placeholder="Password" class=" wt-text-main border tw-p-1 rounded tw-h-9 tw-w-3/4 tw-mb-3 focus:tw-outline-none focus:tw-border-main " />
+                        <input value={inputData.password} onChange={(e) => setInputData({ ...inputData, password: e.target.value })} type="password" placeholder="Password" className=" wt-text-main border tw-p-1 rounded tw-h-9 tw-w-3/4 tw-mb-3 focus:tw-outline-none focus:tw-border-main " />
 
-                        {insideRegister ? 
+                        {insideRegister ?
 
                             <p> Already a user ?<Link to="/login"> Click to login</Link> </p>
                             :
