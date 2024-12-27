46e1e45c69b9d8115babe5d40e0c38651ab3f393
Zavioo <saviotitus63@gmail.com>
2024-12-27 12:29:50 +0530

    EditPost Added

---
+++ src/components/EditPost.jsx
@@ -0,0 +1,184 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { Button, Modal } from 'react-bootstrap';
+import SERVER_URL from '../Services/serverURL';
+
+
+const EditPost = ({ post }) => {
+    console.log(post);
+
+    const [show, setShow] = useState(false);
+    const [preview, setPreview] = useState("")
+    // const [existingMedia, setExistingMedia] = useState()
+    const [imageFileStatus, setImageFileStatue] = useState(false)
+    const fileInputRef = useRef(null); // Ref for the file input
+    const [isVideo, setIsVideo] = useState(false)
+    const user = JSON.parse(sessionStorage.getItem("user"))
+
+    const [postDetails, setPostDetails] = useState({
+        title: "", description: "", media: ""
+    })// for storing postDetails
+
+    const mediaType = post.media.split('.').pop().toLowerCase(); //To Get file extension
+
+    useEffect(() => {
+
+        if (postDetails.media.type == 'image/png' || postDetails.media.type == 'image/jpeg' || postDetails.media.type == 'image/jpg') {
+            setImageFileStatue(true)
+            // createURL method is used to convert file type to url here URL is js class 
+            setPreview(URL.createObjectURL(postDetails.media))
+            // console.log('Inside img useEffect if true');
+
+        } else if (postDetails.media.type == 'video/mp4' || postDetails.media.type == 'video/x-matroska') {
+            setImageFileStatue(true)
+            // createURL method is used to convert file type to url here URL is js class 
+            setPreview(URL.createObjectURL(postDetails.media))
+            setIsVideo(true)
+            // console.log('Inside Video useEffect if true');
+        } else {
+            setIsVideo(false)
+            setImageFileStatue(false)
+            setPreview("")
+            setPostDetails({ ...postDetails, postVideo: "" })
+            // console.log('Inside else if img and video is false ');
+        }
+    }, [postDetails.media])
+
+
+    const handleAddPost = async () => {
+        const { username, profileImg, title, description, media } = postDetails
+
+        if (title && description && media) {
+            // alert("Make api call")
+            const reqBody = new FormData() // reqbody in formdata becuse its includes file
+            reqBody.append("username", username)
+            reqBody.append("profileImg", profileImg)
+            reqBody.append("title", title)
+            reqBody.append("description", description)
+            reqBody.append("media", media)
+            const token = sessionStorage.getItem("token")
+            if (token) {
+                const reqHeader = {
+                    "Content-Type": "multipart/form-data",
+                    "Authorization": `Bearer ${token}`
+                }
+                // make api call
+                try {
+                    const result = await addPostAPI(reqBody, reqHeader)
+                    if (result.status == 200) {
+                        alert("Post added successfully!!!")
+
+                        handleClear()
+                        handleClose()
+
+                    } else {
+                        alert(result.response.data)
+                    }
+
+                } catch (err) {
+                    console.log(err);
+
+                }
+            }
+
+
+        } else {
+            alert("Plzz add all fields")
+        }
+    }
+
+
+    const handleShow = () => setShow(true)
+    const handleClose = () => setShow(false)
+
+    const handleClear = () => {
+        setPostDetails({ ...postDetails, title: "", description: "", media: "" })
+        console.log(postDetails);
+        if (fileInputRef.current) fileInputRef.current.value = ""; // Reset file input
+    }
+
+    const closeAndClear = () => {
+        handleClose()
+        handleClear()
+    }
+
+
+    return (
+        <>
+            <button onClick={handleShow} className="mx-3">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="tw-size-6 tw-text-black"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
+                    />
+                </svg>
+            </button>
+
+            <Modal show={show} onHide={handleClose} size="lg" >
+                <Modal.Header closeButton>
+                    <Modal.Title> Create Post </Modal.Title>
+                </Modal.Header>
+                <div className='m-3'>
+                    <input value={postDetails.title} onChange={(e) => setPostDetails({ ...postDetails, title: e.target.value })} class="form-control form-control-sm " type="text" placeholder=" Title " id="inputSmall" />
+
+                    <textarea value={postDetails.description} onChange={(e) => setPostDetails({ ...postDetails, description: e.target.value })} class="form-control my-3" id="exampleTextarea" placeholder=" What's on your mind " rows="3" style={{ height: "200px;" }} ></textarea>
+                    {/* preview of Upload image or Video */}
+                    <div className='tw-flex tw-items-center tw-justify-center'>
+
+                        {['mp4', 'mkv'].includes(mediaType) || isVideo === true ? (
+                            <video className='mb-3 tw-max-h-52 ' controls src={preview ? preview : `${SERVER_URL}/uploads/${post.media}`} ></video>
+                        ) : (
+                            <img className='mb-3 tw-max-h-52 ' src={preview ? preview : `${SERVER_URL}/uploads/${post.media}`} alt="Media" />
+                        )}
+
+                    </div>
+                    {/* add Image Button  */}
+                    <label>
+                        <input ref={fileInputRef} // Attach ref here 
+                            onChange={(e) => setPostDetails({ ...postDetails, media: e.target.files[0] })} type="file" style={{ display: "none" }} />
+                        <span className="btn btn-sm btn-outline-dark tw-ml-2">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="tw-size-4">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
+                            </svg>
+                        </span>
+
+                        {/* add Video Button  */}
+                        <span className="btn btn-sm tw-ml-2 btn-outline-dark">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="tw-size-4">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h-9A2.25 2.25 0 0 0 2.25 7.5v9a2.25 2.25 0 0 0 2.25 2.25Z" />
+                            </svg>
+                        </span>
+                    </label>
+                    {/* clear button */}
+                    <button onClick={handleClear} className=' btn btn-sm btn-outline-dark tw-ml-2'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="tw-size-4">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+                    </svg>
+                    </button>
+                    {!imageFileStatus &&
+                        <div className='tw-text-main tw-font-light tw-my-2 tw-text-center' > Upload jpeg , jpg , png images <br /> or <br />
+                            mp4 , mkv videos
+                        </div>}
+                </div>
+                <Modal.Footer>
+                    <Button variant="primary" >
+                        Share
+                    </Button>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
+}
+
+export default EditPost
--- src/components/PostCards.jsx
+++ src/components/PostCards.jsx
@@ -3,18 +3,21 @@ import { Link } from 'react-router-dom';
 import { StateContext } from '../ContextApi/StateContext';
 import { Modal, Button } from 'react-bootstrap';
 import SERVER_URL from '../Services/serverURL';
+import EditPost from './EditPost';
 
 
 const PostCards = ({ values }) => {
     const [show, setShow] = useState(false);
     const { sharedState } = useContext(StateContext);
+    const [value,setValue] = useState(false);
 
     const profilePic = values.userId?.profilePic || 'user.jpg'
-    
+
     const handleShow = () => setShow(true);
     const handleClose = () => setShow(false);
 
     const mediaType = values.media.split('.').pop().toLowerCase(); //To Get file extension
+
 
     return (
         <div className={sharedState === 'Initial State' ? "card tw-max-w-sm tw-max-h-max tw-mx-5 tw-mb-5" : "card tw-max-w-xs tw-max-h-fit"}>
@@ -49,26 +52,20 @@ const PostCards = ({ values }) => {
 
                 <h6 className="text-dark my-3">{values.title}</h6>
                 <p>
-                    {values.description}{' '}
-                    {sharedState === 'Initial State' && <Link onClick={handleShow}>Read More</Link>}
+                    {values.description}
+                    {sharedState === 'Initial State' &&
+                        <Link onClick={handleShow}>Read More</Link>}
                 </p>
 
                 {sharedState !== 'Initial State' && (
                     <div>
-                        <button onClick={handleShow} className="mx-3">
+                        {/* Edit Post button */}
+                        <EditPost post={values}/>
+                        {/* Post Delete Button */}
+                        <button>
                             <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="tw-size-6 tw-text-black"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10"
-                                />
+                                xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="tw-size-6">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                             </svg>
                         </button>
                     </div>
