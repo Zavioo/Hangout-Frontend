176194722e9606b2bb5a09e77002f8bafca6cb44
Zavioo <saviotitus63@gmail.com>
2025-01-24 13:47:34 +0530

    To view Saved Posts implimented

--- src/pages/SavedPost.jsx
+++ src/pages/SavedPost.jsx
@@ -1,32 +1,59 @@
-import React, { useEffect } from 'react'
-import { getSavedPostAPI } from '../Services/allApi'
+import React, { useContext, useEffect, useState } from 'react'
+import { PostResponseContext } from '../ContextApi/StateContext';
+import { allPostAPI } from '../Services/allApi';
+import PostCards from '../components/PostCards';
+import { Link } from 'react-router-dom';
 
 const SavedPost = () => {
+    const { postResponse } = useContext(PostResponseContext)
+    const [allPosts, setAllPosts] = useState([])
     const user = JSON.parse(sessionStorage.getItem("user"))
-    const id = user._id
+    const userId = user._id
+    
+    
+
 
     useEffect(() => {
-        handleGetSavedPosts()
-    }, [])
+        fetchPosts();
+    }, [postResponse]);
 
-
-    const handleGetSavedPosts = async () => {
+    const fetchPosts = async () => {
+        const searchKey = ""
         try {
-            const result = await getSavedPostAPI(id)
-            
-            if (result.status == 200) {
-                console.log(result.data);
-
+            const result = await allPostAPI(searchKey);
+            if (result.status === 200) {
+                setAllPosts(result.data);
+                
             }
         } catch (err) {
-            console.error('Error Getting Saved Posts', err);
+            console.error('Error fetching posts:', err);
         }
+    };
 
-
-    }
 
     return (
-        <div>SavedPost</div>
+        <div >
+            <header className='tw-p-5 tw-mb-2 tw-shadow-md tw-grid tw-grid-cols-3 '>
+                <div>
+
+                </div>
+                <h2 className='tw-text-center'>Saved Posts
+                </h2>
+                <div className=' tw-flex tw-justify-end'> <Link to='/userhome' className='btn btn-outline-dark'>Back</Link></div>
+            </header>
+            <div className='tw-px-10'>
+                <div style={{ maxHeight: "85vh" }} className='tw-shadow-md tw-overflow-x-auto tw-grid tw-grid-cols-3'>
+                    {allPosts.length > 0 && (
+                        allPosts.map((post, index) => {
+                            if (post.savedUsers.includes(userId)) {
+                                return(<PostCards key={index} values={post} />)
+                            }
+                        })
+                    )
+                    }
+                </div>
+            </div>
+        </div>
     )
 }
 
